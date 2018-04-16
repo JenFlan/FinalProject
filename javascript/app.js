1747202858f41d69066955b5c35eb4bd87944002
JenFlan <36905514+JenFlan@users.noreply.github.com>
2018-04-16 09:38:09 -0600

    updates to all pages css js

---
+++ javascript/app.js
@@ -0,0 +1,15 @@
+const a = 1
+
+console.log (a)
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
+  });
+
+
+

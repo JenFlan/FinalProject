bdb56fbbf0023a461a20d39dd2e18feb6d508f11
JenFlan <36905514+JenFlan@users.noreply.github.com>
2018-04-29 17:26:59 -0600

    JS updates, table, sponsorform

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,51 @@
-const a = 1
+const a = 1;
 
-console.log (a)
+console.log(a);
 
 
+/*
+const remindFormOpen = () => {
+	const $confremindbtn = $('.confremindbtn');
+	const $confremindform = $('.confremindform');
+
+	$confremindbtn.on('click', e => {
+	$confremindform.removeClass('hidden')
+
+});
+*/
+
+/*
+const remindFormOpen = () => {
+	const $confremindbtn = $('.confremindbtn');
+	const $confremindform = $('.confremindform');
+
+	$confremindbtn.on('click', e => {
+	$confremindform.removeClass('hidden')
+
+});
+
+
+
+*/
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "order": [[ 0, "asc" ]]
+    } );
+});
+
+
+/*
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 2,
     center: new google.maps.LatLng(2.8,-187.3),
     mapTypeId: 'terrain'
-  });
-/*
+  }); 
+}
+
 $(document).ready( function () {
     $('#table_id').DataTable();
 } );
 */
-
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "order": [[ 3, "desc" ]]
-    } );
-} );
